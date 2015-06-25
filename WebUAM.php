23cf6182614defcf04b00633ab2b13d04638963e
dimkasta <dimkasta@dimkasta.net>
2015-06-26 02:59:07 +0300

    Update WebUAM.php

--- WebUAM.php
+++ WebUAM.php
@@ -249,7 +249,7 @@
 				$subject = $f3->site . " - Email Verificaton";
 $txt = "You received this email because you have requested to " . $message . " at ". $f3->site . "\n Please click the link below to verify it\nhttp://" . $f3->domain . "/" . $f3->emailverificationroute . "?email=" . $email . "&token=" . $token;
 				$headers = "From: " . $f3->email;
-				//mail($email,$subject,$txt,$headers);
+				mail($email,$subject,$txt,$headers);
 		}
 
 		
