5ecd19198f3e57a4ca8bf903e5c04395cc4e8284
dimkasta <dimkasta@dimkasta.net>
2015-06-29 01:59:37 +0300

    Update Users.php

--- Users.php
+++ Users.php
@@ -1,45 +1,33 @@
 <?php
 //Sample Controller. You need routes for each one of the functions below
-
 namespace Controller;
 
 class Users {
-
 	public static function subscribe($f3) {
-    		//Show the subscription form
+    		//Show subscription form
 	}
     
-  //You can call this with AJAX or internally
-	public static function doSubscribe() {
-		$f3 = \Base::instance();
+	public static function doSubscribe($f3) {
 		$params = json_decode(file_get_contents('php://input'));
-		$json = $f3->uam->doSubscription($params->username, $params->email, $params->password);
-    		\RESTAPI::returnJSON($json);
+		\RESTAPI::returnJSON($f3->uam->doSubscription($params->username, $params->email, $params->password));
 	}
 	
-	//You can call this with AJAX or internally
-	public static function validateEmail() {
+	public static function validateEmail($f3) {
 		$f3 = \Base::instance();
 		$f3->uam->validateEmail();
 		$f3->reroute('/login');
 	}
 	
-	
 	public static function login() {
-		//Show the login form
+		//Show login form
 	}
 	
-	//You can call this with AJAX or internally
-	public static function doLogin() {
-		$f3 = \Base::instance();
+	public static function doLogin($f3) {
 		$params = json_decode(file_get_contents('php://input'));
-		$json = $f3->uam->doLogin($params->username, $params->password);
-		\RESTAPI::returnJSON($json);
+		\RESTAPI::returnJSON($f3->uam->doLogin($params->username, $params->password));
 	}
 	
-	
-	public static function logout() {
-		$f3 = \Base::instance();
+	public static function logout($f3) {
 		$f3->uam->doLogout();
 		$f3->reroute('/');
 	}
